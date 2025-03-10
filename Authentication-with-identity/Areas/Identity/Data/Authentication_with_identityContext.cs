7b46cc3c9a689b9da2284fa24f622f3e6832f363
matthieu-vb <m.j.van.bekkum@windesheim.nl>
2025-03-10 10:13:21 +0100

    Initial commit
    Leuk projectje.

---
+++ Authentication-with-identity/Areas/Identity/Data/Authentication_with_identityContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication_with_identity.Data;
+
+public class Authentication_with_identityContext : IdentityDbContext<IdentityUser>
+{
+    public Authentication_with_identityContext(DbContextOptions<Authentication_with_identityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
